e57b0c9619dfe380360e1dd098f81d03195d2206
PratikKumar-6206 <pratikpatna18@gmail.com>
2024-07-28 15:04:13 +0530

    add one file "test2"

---
+++ basic/test2.js
@@ -0,0 +1,119 @@
+console.log("hello world");
+console.log("I'm pratik");
+console.log("how vare you");
+console.log(1234);
+console.log(7+8);
+// console.log("6+8"); this is used as text/char
+// variable
+// float and double store real Number and int store Node./
+a=12.8;
+console.log(a+11);
+console.log(a-8);
+x="pratik";
+console.log(x);
+a=2;
+b=4;
+c=5;
+console.log(a,b,c);
+nam="pratik"
+console.log("my name is",nam);
+age=22;
+console.log(age);
+console.log("my age is",age);
+var a=42;
+console.log(a);
+a=12;
+console.log(a);
+//const b=23; //we can't chane value of this were its upper part of code will run but niche wale nhi chalega jab mai const ke value ko change karung
+//console.log(b);
+//data type
+a=3;
+b=3.4;
+c="pratik";
+d=true;
+e='p';
+console.log(a,typeof(a));
+console.log(b,typeof(b));
+console.log(c,typeof(c));
+console.log(d,typeof(d));
+console.log(e,typeof(e));
+//if else 
+age=23;
+if(age>=18) console.log("adult");
+else console.log("not adult");
+        //or
+    //    if(age>=18){
+    //         console.log("adult");
+    //     }
+    //     else{
+    //         console.log("not adult");
+    //     }
+        
+    //No.check
+    num=7;
+    if(num%2==0){
+        console.log("even number");
+    }
+    else console.log("odd number");
+    num%2==0?console.log("shi h"):console.log("galat h");
+
+    (num%2==0 &&<p>pritik</p>)
+
+    //rating
+    rating=4;
+    // 1-poor
+    // 2-belowavg
+    // 3-avg 
+    // 4-good 
+    // 5-exellent
+if(rating==1) console.log('poor');
+else if(rating==2) console.log('below avg');
+else if(rating==3) console.log('avg');
+else if(rating==4) console.log('good');
+else console.log("exellent");
+
+//check three digit number
+x=34;
+if(x>=100 && x<=999) console.log(x,"is a 3 digit number");
+else console.log('not a three digit number');
+
+//== its only check value not its type
+//=== it check its value and its type
+a=4;
+b='4';
+console.log(a==b);
+console.log(a===b);
+//loop
+for(let i=1;i<=10;i++){
+    console.log(i); //usually let is use in loop
+}
+
+//while loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+//do while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=14); //even though its not fullfill the condition but it run at least one time
+
+//array
+arr=[1,2,3,4,5];
+console.log(arr);
+n=arr.length;
+console.log(n);
+arr.push(6); //add at last
+console.log(arr);
+arr.pop(); //remove from last
+console.log(arr);
+arr.unshift(8); //add frome first
+console.log(arr);
+arr.shift(); //remove from first
+console.log(arr);
+
+//
+
